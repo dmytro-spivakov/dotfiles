f92609c8ff5fadde3e9bada67be0f3902e68f3ec
Dmytro Spivakov <dmytro_spivakov@proton.me>
2025-09-11 18:04:37 +0300

    update dotfiles

--- init.lua
+++ init.lua
@@ -238,6 +238,15 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  -- show indentation guides
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+  },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
